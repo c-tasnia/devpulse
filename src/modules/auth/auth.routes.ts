a19563a2d678739a2d077db81999f9353ba31659
c-tasnia <tcpuc1042@gmail.com>
2026-05-23 22:25:02 +0600

    Create auth.routes.ts

---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { signup, login } from './auth.controller';
+
+const router = Router();
+
+
+router.post('/signup', signup);
+
+
+router.post('/login', login);
+
+export default router;
